9294518e05f53fef379c86e5033e6c5fea91a38c
Jon Finerty <jonathanfinerty@gmail.com>
2019-09-14 18:28:59 +0100

    First commit :baby:

---
+++ tree.js
@@ -0,0 +1,35 @@
+var treeData =
+[
+  {
+    "name": "Start Company",
+    "parent": "null",
+    "children": [
+      {
+        "name": "Level 2: A",
+        "parent": "Start Company",
+        "children": [
+          {
+            "name": "Buy Machines",
+            "parent": "Level 2: A"
+          },
+          {
+            "name": "Deployment",
+            "parent": "Level 2: A"
+          },
+          {
+            "name": "Process",
+            "parent": "Level 2: A"
+          },
+          {
+            "name": "Deployment",
+            "parent": "Level 2: A"
+          }
+        ]
+      },
+      {
+        "name": "Level 2: B",
+        "parent": "Start Company"
+      }
+    ]
+  }
+]
